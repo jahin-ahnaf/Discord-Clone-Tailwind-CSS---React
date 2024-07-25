842abd40520b6af29897b8bd2dca5d1efa786bdc
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-07-25 14:02:08 +0600

    first commit

---
+++ React + Tailwind CSS/src/App.jsx
@@ -0,0 +1,12 @@
+import SideBar from "./SideBar"
+
+function App() {
+
+  return (
+    <>
+      <SideBar/>
+    </>
+  )
+}
+
+export default App
---
+++ React + Tailwind CSS/src/SideBar.jsx
@@ -0,0 +1,51 @@
+export default function SideBar() {
+  return (
+    <div className="fixed top-0 left-0 flex flex-col h-screen m-0 text-white shadow-lg bg-zinc-900 w-16">
+      <SidebarIcon
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        }
+      />
+      <SidebarIcon
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        }
+      />
+      <SideBar icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>
+
+}/>
+    </div>
+  );
+}
+
+function SidebarIcon({ icon }) {
+  return <div className="sidebar-icon">{icon}</div>;
+}
